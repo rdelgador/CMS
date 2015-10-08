e85a43ed425a5feda76fb0d59d1ee0573654d520
rdelgador <ricardo.delgado@ucsp.edu.pe>
2015-10-07 23:32:01 -0500

    #8 Agregado change_pass

--- app/authentication/controllers.py
+++ app/authentication/controllers.py
@@ -10,11 +10,50 @@ from flask import (
 )
 from werkzeug import check_password_hash, generate_password_hash
 
-from app import db
-from app.authentication.forms import LoginForm
+from app import db, mail
+from app.authentication.forms import LoginForm, SignupForm, RecoverPassForm, ResetPasswordSubmit
 from app.authentication.models import User
+from flask.ext.security.utils import encrypt_password, verify_password
+from flask_mail import Mail, Message
+
+
 
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+@mod_auth.route('/profile')
+def profile():
+
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+
+    user = User.query.filter_by(email = session['email']).first()
+
+    if user is None:
+        return redirect(url_for('signin'))
+    else:
+        return render_template('authentication/profile.html')
+
+@mod_auth.route('/signup/', methods=['GET', 'POST'])
+def signup():
+
+    form = SignupForm()
+
+    if 'email' is session:
+        return redirect(url_for('profile'))
+    
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template("authentication/signup.html", form=form)
+        else:
+            new_user = User(form.username.data, form.email.data, form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['email'] = new_user.email
+            return "Not found"
+        
+    elif request.method == 'GET':
+        return render_template("authentication/signup.html", form=form)
 
 
 @mod_auth.route('/signin/', methods=['GET', 'POST'])
@@ -31,3 +70,18 @@ def signin():
         flash('Wrong email or password', 'error-message')
 
     return render_template("authentication/signin.html", form=form)
+
+@mod_auth.route('/change_pass', methods=['GET','POST'])
+def change_pass():
+	token = request.args.get('token',None)
+	verified_result = User.verify_token(token)
+	if token and verified_result:
+		print verified_result
+		password_submit_form = ResetPasswordSubmit(request.form)
+
+		if password_submit_form.validate_on_submit():
+			verified_result.password = generate_password_hash(password_submit_form.password.data)
+			db.session.commit()
+			flash("password updated successfully")
+			return redirect('users')
+		return render_template("change_pass.html",form=password_submit_form)
